7000e172bad5c40516c7e7000678ac9578a54e87
ruben <462404@student.fontys.nl>
2021-10-18 13:36:05 +0200

    Setup Vue Project

---
+++ musix-frontend/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Strait !important;
+}
+
+:root{
+  --primary-color: rgba(248, 38, 38, 1);
+  --secondary-color: rgb(255, 255, 255, 1);
+  --text-color: rgb(0, 0, 0, 0.85);
+}
+</style>
