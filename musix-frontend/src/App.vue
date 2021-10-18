c41e30b7395c7424f0f4eadb8539709972efd6a6
ruben <462404@student.fontys.nl>
2021-10-18 13:36:13 +0200

    Add Background

--- musix-frontend/src/App.vue
+++ musix-frontend/src/App.vue
@@ -1,11 +1,14 @@
 <template>
+  <Background />
 </template>
 
 <script>
+import Background from "./components/Background.vue"
 
 export default {
   name: 'App',
   components: {
+    Background
   }
 }
 </script>
