5db80174f5cc49320c22505a7ccc2412107e86e4
ruben <462404@student.fontys.nl>
2021-10-20 16:38:50 +0200

    Setup Registration

---
+++ musix-frontend/src/wrappers/RegistrationWrapper.js
@@ -0,0 +1,24 @@
+import axios from "axios";
+
+// Base url can be found in the .env file in the root folder
+export const baseUrl = process.env.VUE_APP_API_BASE_URL;
+
+export default {
+
+    async signUp(data) {
+
+        var response;
+
+        try{
+            response =  await axios.post(`${baseUrl}/api/Registration`, data)
+        }
+        catch(error){
+            response = error.response
+        }
+
+        return {
+            success: response.data["success"],
+            message: response.data["message"]
+        }
+    }
+} 
