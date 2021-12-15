3bac1e17f5b712e1a104848750b5f00604ae6ce1
ruben <462404@student.fontys.nl>
2021-12-15 16:01:04 +0100

    Update vue.config.js

--- musix-frontend/vue.config.js
+++ musix-frontend/vue.config.js
@@ -1,5 +1,5 @@
 const releaseType = process.env.VUE_APP_RELEASE_TYPE
 
 module.exports = {
-     publicPath = releaseType === 'production' ? '/MusiX-FrontEnd/' : '/'
+     publicPath: releaseType === 'production' ? '/MusiX-FrontEnd/' : '/'
 }
